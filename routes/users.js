7d3bd5d5d5d82f28fe164349db1225822d65e1ab
Naseeb Sidan <118831644+naseebsidan7@users.noreply.github.com>
2023-08-04 21:02:03 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -76,7 +76,7 @@ router.get('/deleteAddress',auth.is_login,userController.deleteAddress)
 router.get('/getAddress', userController.getAddress);
 
 router.get('/cart',auth.is_login,userController.showCart)
-router.post('/addtocart',userController.addtocart)
+router.post('/addtocart',auth.is_login,userController.addtocart)
 router.post('/update-cart', userController.updateCart);
 router.get('/deleteCartProduct',auth.is_login,userController.deleteCartProduct)
  
